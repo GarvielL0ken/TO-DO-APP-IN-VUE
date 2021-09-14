b6458df6479f0138077b039675bd9adc5812bcbc
GarvielL0ken <garviel.loken.42@gmail.com>
2021-09-14 15:18:02 +0200

    All fields can now be edited via the 'edit' display mode

--- todoapp.js
+++ todoapp.js
@@ -1,25 +1,31 @@
 new Vue({
 	el : "#todoapp",
 	data : {
-		newTask : "test",
+		newTaskLabel : "test",
+		newTaskCategory : "",
+		newTaskDueDate : "",
+		newTaskPriority : 0,
 		state : "view",
 		tasks : [{label : "Complete Project",
 					category : "Career",
+					dueDate : "1999-11-15",
 					priority : 0,
 					complete : false},
 				{label : "Walk the Dog",
 					category : "Household",
+					dueDate : "1999-11-15",
 					priority : 1,
 					complete : false},
 				{label : "Wash the dishes",
 					category : "Household",
+					dueDate : "1999-11-15",
 					priority : 2,
 					complete : false}]
 	},
 	methods : {
 		addTask() {
-			this.tasks.push({label : this.newTask, category : "None", priority : 0, complete : false})
-			this.newTask = ""
+			this.tasks.push({label : this.newTaskLabel, category : "None", priority : 0, complete : false})
+			this.newTaskLabel = ""
 		},
 		changeState(newState) {
 			this.state = newState
